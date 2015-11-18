6cd56f8df9ec5592c29ac4c57217777c2d2c18cb
helloaihao <543160349@qq.com>
2015-11-18 21:07:34 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -11,47 +11,83 @@ var $           = require('gulp-load-plugins')();
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 
+
+var web = './LinkEOL.Website/';
+
+//路径
 var globs = {
-	js: 'app/js/**/*.js',
-	css: 'app/css/**/*.less',
-	less: 'app/less/**/*.less',
-	html: 'app/**/*.html',
+	js: web + 'script/**/*.js',
+	css: web + 'css/**/*.css',
+	less: web + 'less/**/*.less',
+	html: web + '**/*.html',
+	rev: web + 'rev/**/*.json',
 	assets: [
-		'app/fonts/**/*',
-		'app/images/**/*'
+		web + 'fonts/**/*',
+		web + 'images/**/*'
 	]
 }
 
+//压缩CSS
 gulp.task('css', function() {
 	gulp.src(globs.css)
 	.pipe($.minifyCss())
-	.pipe($.rename(function(path) {
-		path.basename = path.basename.replace(path.basename, path.basename + '.min');
-	}))
-	.pipe(gulp.dest(''));
+	.pipe($.rename({suffix: '.min'}))
+	.pipe($.rev())
+	.pipe(gulp.dest(web + 'css/'))
+	.pipe($.rev.manifest())
+	.pipe(gulp.dest(web + 'rev/css'))
 });
+
 
 gulp.task('js', function() {
 	gulp.src(globs.js)
 	.pipe($.uglify())
-	.pipe($.rename(function(path) {
-		path.basename = path.basename.replace(path.basename, path.basename + '.min');
-	}))
+	.pipe($.rename({suffix: '.min'}))
 	.pipe(gulp.dest(''));
 });
 
+//编译less
 gulp.task('less', function() {
-	gulp.src(globs.less)
+	gulp.src(web + 'less/main.less')
 	.pipe($.less())
-	.pipe(gulp.dest('app/css'))
+	.pipe(gulp.dest(web + 'css'))
 	.pipe(reload({stream: true}));
+});
+
+gulp.task('rev', function(){
+	gulp.src([globs.rev, web + 'index.html'])
+	.pipe($.revCollector({
+		replaceReved: true
+	}))
+	.pipe(gulp.dest(web + ''));
+});
+
+gulp.task('index', function() {
+	var jsFilter = $.filter(globs.js, {restore: true});
+	var cssFilter = $.filter(globs.css, {restore: true});
+
+	//var userefAssets = $.useref.assets();
+
+	return gulp.src(web + 'index.html')
+    //.pipe(userefAssets)  // 解析html中build:{type}块，将里面引用到的文件合并传过来
+    .pipe(jsFilter)
+    .pipe($.uglify())             // 压缩Js
+    .pipe(jsFilter.restore)
+    .pipe(cssFilter)
+    .pipe($.minifyCss())               // 压缩Css
+    .pipe(cssFilter.restore)
+    .pipe($.rev())                // 重命名文件
+    //.pipe(userefAssets.restore())
+    .pipe($.useref())
+    .pipe($.revReplace())         // 重写文件名到html
+    .pipe(gulp.dest(web + 'dist'));
 });
 
 // 静态服务器
 gulp.task('serve', function() {
     browserSync.init({
         server: {
-            baseDir: './app'
+            baseDir: web
         }
     });
 
